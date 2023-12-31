bd93abe3f21444cd0b71b063ac2b3ca2c8f2e73c
Mikayla Sagle <msagle12@gmail.com>
2023-12-31 15:17:42 -0500

    remove outer file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      THIS IS GOING TO BE ELEVATE
+    </div>
+  );
+}
+
+export default App;
