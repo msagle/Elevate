83f57603b9249d4d82e3f9759bc925d2d64126f5
Mikayla Sagle <msagle12@gmail.com>
2024-02-18 12:46:06 -0500

    marquee working fine enough

--- src/pages/CampsPage/CampsPage.tsx
+++ src/pages/CampsPage/CampsPage.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function CampsPage() {
     return (
         <div>
-            CAMPS PAGE LETS GO!
+            <h1>CAMPS</h1>
+            <p>
+                Registration for the 2024 ELEVATE Synchro Camp is now live! Join us for 2 days of on-ice and off-ice training this July in Pleasant Prarie, WI!
+            </p>
         </div>
         
     );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,9 +6,38 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 export default function HomePage() {
     return (
-        <div className={styles.carousel}>
-            <img src={ThreeSkates} alt={"Three skates"}/>
-             <h3 style={{color: 'black'}}>Dedicated to inspiring and educating the next generation of synchronized skaters</h3>
+        <div>
+            <div className={styles.marquee}>
+                <div className={styles.marqueeone}>
+                    <p>
+                        Registration for the 2024 ELEVATE Synchro Camp is now open! Come train with us July 12th to the 14th!
+                    </p>
+                </div>
+                <div className={styles.marqueetwo}>
+                    <p>
+                        Registration for the 2024 ELEVATE Synchro Camp is now open! Come train with us July 12th to the 14th!
+                    </p>
+                </div>
+                <div className={styles.marqueethree}>
+                    <p>
+                        Registration for the 2024 ELEVATE Synchro Camp is now open! Come train with us July 12th to the 14th!
+                    </p>
+                </div>
+                <div className={styles.marqueefour}>
+                    <p>
+                        Registration for the 2024 ELEVATE Synchro Camp is now open! Come train with us July 12th to the 14th!
+                    </p>
+                </div>
+            </div>
+            
+            <br />
+            <br />
+            <br />
+
+            <h3 style={{color: 'black'}}>Dedicated to inspiring and educating the next generation of synchronized skaters</h3>
+            <p>
+                why is this bugging? yaaaaa
+            </p>
         </div>
     );
 };
