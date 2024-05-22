50e856fe4052e1f646adda0ea4bb1e9777850dcc
Mikayla Sagle <msagle12@gmail.com>
2024-05-22 17:07:46 -0400

    privacy policy pdf working

--- src/components/RoutingBarFooter.tsx
+++ src/components/RoutingBarFooter.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
+//import privacyPolicyPDF from "../../public/docs/PrivacyPolicy.pdf";
 import styles from "./RoutingBar.module.css";
+import pdf from "../types.s";
+
+const privacyPDF = pdf("/docs/PrivacyPolicy.pdf");
 
 export default function RoutingBarFooter() {
     return (
@@ -9,7 +13,10 @@ export default function RoutingBarFooter() {
             <Link to={"/mission"}><strong>About Us</strong></Link> 
             <Link to={"/team"}><strong>Our Team</strong></Link> 
             <Link to={"/camps"}><strong>Camps</strong></Link> 
-            <Link to={"/privacy"}><strong>Privacy Policy</strong></Link> 
+            <a href={privacyPDF} target="_blank"
+                    rel="noreferrer"> 
+                    <strong>Privacy Policy</strong>
+            </a>
             <Link to={"https://www.instagram.com/elevatesynchro/"}><FaInstagram /></Link>
             <Link to={"https://www.facebook.com/elevatesynchro/"}><FaFacebook /></Link>
         </div>
