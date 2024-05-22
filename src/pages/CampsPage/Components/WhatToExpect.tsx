f8c575cc1a78b54043faa5c74e3d987191ef82e7
Mikayla Sagle <msagle12@gmail.com>
2024-05-22 01:19:46 -0400

    css updates and improvements

--- src/pages/CampsPage/Components/WhatToExpect.tsx
+++ src/pages/CampsPage/Components/WhatToExpect.tsx
@@ -10,17 +10,39 @@ export default function WhatToExpect() {
             <div className={styles.whatToExpectContainer}>
                 <div className={styles.whatToExpectInformationContainer}>
                     <p>
-                        This year, the camp is organized into three distinct groups to cater to skaters of varying skill level depending on group placement: 
-                        ASPIRE, Pre-Juvenile - Intermediate Skating Skills Levels, and Novice - Junior/Senior Skating Skills Levels. 
-                        The camp is two days long for both the Pre-Juvenile - Intermediate and Novice - Junior/Senior groups while the camp is only a 1/2 day experience for the ASPIRE group. 
+                        This year, the camp is organized into three distinct groups to cater to skaters of varying skill level depending on group placement:
+                        <ul>
+                            <li><strong><span>ASPIRE to Pre-Juvenile</span></strong></li>
+                            <li><strong><span>Juvenile to Intermediate Skating Skills Levels</span></strong></li>
+                            <li><strong><span>Novice to Junior Skating Skills Levels</span></strong></li>
+                        </ul>
+                        The camp is two days long for both the Juvenile - Intermediate and Novice - Junior groups while the camp is only a half day experience for the ASPIRE - Pre-Juvenile group. 
                         Below are the dates and times when each level will be taking place: 
                         <br />
                         <br />
-                        <strong>ASPIRE - Pre-Juvenile:</strong> Saturday, July 13th - 8:00AM - 12:30PM <br/>
-                        <strong>Juvenile - Intermediate Skating Skills Levels:</strong> Saturday, July 13th - 11:30AM-6:45PM & Sunday, July 14th - 9:00AM-3:00PM <br/>
-                        <strong>Novice - Junior Skating Skills Levels:</strong> Friday, July 12th - 2:00PM-7:30PM & Saturday, July 13th 9:15AM-6:00PM <br/>
-                        <br />
-                        The camp consists of a little over 13 hours of on- ice training for the latter two groups and 1.5 hours of on-ice training for the ASPIRE group. 
+                        <ul>
+                            <li><strong>ASPIRE - Pre-Juvenile:</strong>
+                                <li>Saturday, July 13th - 8:00AM - 12:30PM</li>
+                            </li>
+                            <br />
+                            <li><strong>Juvenile - Intermediate Skating Skills Levels:</strong>
+                                <li>Saturday, July 13th from 11:30AM to 6:45PM</li>
+                                <li>Sunday, July 14th from 9:00AM to 3:00PM</li>
+                            </li>
+                            <br />
+                            <li><strong>Novice - Junior Skating Skills Levels</strong>
+                                <li>Friday, July 12th from 2:00PM to 7:30PM</li>
+                                <li>Saturday, July 13th from 9:15AM to 6:00PM</li>
+                            </li>
+                        </ul>
+                    </p>
+                    <div id={styles["insta"]}>
+                        <InstagramEmbed url="https://www.instagram.com/p/C4yh-2FxHa0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={350}/>
+                    </div>
+                </div>
+                <div>
+                    <p>
+                        The camp consists of a little over 13 hours of on- ice training for the latter two groups and 1.5 hours of on-ice training for the ASPIRE - Pre-Juvenile group. 
                         On the ice, we will be working on various essential synchro skills like turns, twizzles, pair elements, and general skating skills. 
                         In past camps, we have worked on artistry, dance, team building, stretching, learning judging system rules, and sports psychology during off-ice sessions.
                         <br />
@@ -30,11 +52,8 @@ export default function WhatToExpect() {
                         <br />
                         <br />
                         We hope that these answer a few of the questions you may have about this year's camp. If you have any more questions or concerns, 
-                        please feel free to reach out via email at <strong>elevatesynchro@gmail.com</strong>!
+                        please feel free to reach out via email at <span><strong>elevatesynchro@gmail.com</strong></span>!
                     </p>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <InstagramEmbed url="https://www.instagram.com/p/C4yh-2FxHa0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" width={350}/>
                 </div>
             </div>
         </div>
