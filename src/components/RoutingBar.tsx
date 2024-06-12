0ebb8efa59a7106efb177a718c5c6a5de90d04a1
Mikayla Sagle <msagle12@gmail.com>
2024-06-12 13:48:09 -0400

    header now has menu dropdown  for mobile

--- src/components/RoutingBar.tsx
+++ src/components/RoutingBar.tsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import styles from "./RoutingBar.module.css";
+import { useState } from 'react';
 
-export default function RoutingBar() {
+export default function RoutingBar(props: RoutingBarProps) {
+
+    const handleOnClick = () => {
+        props.setDisplay(!props.display);
+    };
+
     return (
-        <div className={styles.linkContainer}>
-            <Link to={"/"}><strong>Home</strong></Link> 
-            <Link to={"/mission"}><strong>About Us</strong></Link> 
-            <Link to={"/camps"}><strong>Camps</strong></Link> 
-            <Link to={"/contact"}><strong>Contact Us</strong></Link> 
+        <div className={props.display? styles.linkContainerMenu : styles.linkContainer}>
+            <Link to="/" onClick={handleOnClick}><strong>Home</strong></Link>
+            <Link to="/mission" onClick={handleOnClick}><strong>About Us</strong></Link>
+            <Link to="/camps" onClick={handleOnClick}><strong>Camps</strong></Link>
+            <Link to="/contact" onClick={handleOnClick}><strong>Contact Us</strong></Link>
         </div>
     );
 };
+
+export interface RoutingBarProps {
+    display: boolean;
+    setDisplay: React.Dispatch<React.SetStateAction<boolean>>;
+}
