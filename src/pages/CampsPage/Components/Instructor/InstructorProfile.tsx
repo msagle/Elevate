b5829414990ed4bb7bbdb2e2ad3b970e0189ff58
Mikayla Sagle <msagle12@gmail.com>
2024-06-12 00:09:56 -0400

    bios modal working for instructors

--- src/pages/CampsPage/Components/Instructor/InstructorProfile.tsx
+++ src/pages/CampsPage/Components/Instructor/InstructorProfile.tsx
@@ -1,35 +1,40 @@
 import { useState } from 'react';
 import styles from './InstructorProfile.module.css';
 import { Instructors } from './Instructors';
-import LauraBio from "./Bios/LauraBio24.png";
+import { Modal } from 'react-bootstrap';
 
 
 export default function InstructorProfile(props: InstructorProfileProps) {
-    const [displayBio, setDisplayBio] = useState<Boolean>(true);
+    const [displayBio, setDisplayBio] = useState<boolean>(false);
 
     const toggleBio = () => {
         setDisplayBio(!displayBio);
     };
 
     return ( 
-        <div onClick={toggleBio}>
-                {displayBio && (
-                    <div className={styles.instructorProfileContainer}>
-                        <div className={styles.instructorImage}>
-                            <img src={props.profile.picture} alt="Laura" />
-                        </div>
-                        <div className={styles.instructorInfo}>
-                            <h3>{props.profile.name}</h3>
-                            <h6>{props.profile.team}</h6>
-                        </div>
+        <div>
+            <div onClick={toggleBio} className={styles.instructorProfileContainer}>
+                <div className={styles.instructorImage}>
+                    <img src={props.profile.picture} alt={props.profile.name} />
+                </div>
+                <div className={styles.instructorInfo}>
+                    <h3>{props.profile.name}</h3>
+                    <h6>{props.profile.team}</h6>
+                </div>
+            </div>
+
+            <Modal show={displayBio} 
+                   onHide={toggleBio}
+                   aria-labelledby="contained-modal-title-vcenter"
+                   centered>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className={styles.bios}>
+                        <img src={props.profile.bio} alt={props.profile.name + "Bio"} />
                     </div>
-                )}
-                {!displayBio && (
-                    <div>
-                        <p>lauraBio!!!</p>
-                        <img src={LauraBio} alt="Laura" />
-                    </div>
-                )} 
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
