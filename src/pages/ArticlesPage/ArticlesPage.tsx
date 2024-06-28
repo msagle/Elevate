0a6a795957f402e3966ecd48fa5b6e5454bb8aaa
Mikayla Sagle <msagle12@gmail.com>
2024-06-27 23:13:43 -0400

    spacing

--- src/pages/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ArticlesPage.tsx
@@ -5,8 +5,10 @@ import ArticlePreviewBox from './ArticlePreviewBox';
 export default function ArticlesPage() {
     return (
         <div className={styles.articlePageContainer}>
-            <h1>ELEVATING the Voices of Synchro</h1>
-            <p>Check out some ELEVATE exclusive articles written by synchronized skaters for synchronized skaters!</p>
+            <div>
+                <h1>ELEVATING the Voices of Synchro</h1>
+                <p>Check out some ELEVATE exclusive articles written by synchronized skaters for synchronized skaters!</p>
+            </div>
             <div className={styles.articleBoxContainer}>
                 {articleList.map((art) => (
                     <ArticlePreviewBox article={art}/>
